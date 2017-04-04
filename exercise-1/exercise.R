5de2b04dc2086dbca7b002868c9fc9edb4c8ef1c
Melody Yu <yu.musicssis@gmail.com>
2017-04-04 15:01:20 -0700

    exercise m7

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,15 +1,25 @@
 # Exercise 1: writing and executing functions
 
 # Write a function `AddThree` that adds 3 to an input value
-
+AddThree <- function(a) {
+  result <- a + 3
+  return(result)
+}
 
 # Create a variable `ten` by passing 7 to your `AddThree` function
-
+ten <- AddThree(7)
 
 # Write a function `FeetToMeters` that converts from feet to meters
-
+FeetToMeters <- function(a) {
+  result <- a * 0.3048
+  return(result)
+}
 
 # Create a variable `height.in.feet` that is your height in feet
-
+height.in.feet <- 544.619
 
 # Create a variable `height.in.meters` by passing `height.in.feet` to your `FeetToMeters` function
+hight.in.meter <- FeetToMeters(height.in.feet)
+
+
+
